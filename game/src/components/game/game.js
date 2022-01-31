1489b1ca2213077aecad4eae0930fb53cff1c64f
Герман Подаков <podakov@gmail.com>
2022-01-31 18:48:25 +0300

    game render

--- game/src/components/game/game.js
+++ game/src/components/game/game.js
@@ -1,11 +1,22 @@
-import React, {useEffect} from 'react';
-import {useDispatch} from "react-redux";
+import React, {useEffect, useRef} from 'react';
+import {useDispatch, useSelector} from "react-redux";
 import * as actionQuest from '../../redux/action/quest';
 import axios from "axios";
+import {Button} from "react-bootstrap";
+import Modal1 from "./modal"
 
 const Game = () => {
+    const [modalShow, setModalShow] = React.useState(false);
+    const [currentQuestId, setCurrentQuestId] = React.useState({});
     const dispatch = useDispatch();
 
+    const {themes} = useSelector((state) => state.questReducer);
+    const ref = useRef();
+    const onPriceClick = (event) => {
+        //(event) => setCurrentQuestId(event);
+            //return setModalShow(true)
+        console.log(event)
+    }
     const onInitQuest = async () => {
         dispatch(actionQuest.initQuestLoading());
         try {
@@ -31,12 +42,34 @@ const Game = () => {
     }
 
     useEffect(()=>{
-        onInitQuest()
-    })
+        console.log(currentQuestId.target)
+    }, [currentQuestId])
+
+    useEffect(()=>{
+        onInitQuest();
+    }, [dispatch])
 
     return (
-        <div>
-            
+        <div className="container">
+            <Modal1
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
+            <div>
+                <table>
+                    {/*<tr>*/}
+                    {/*    <th>&nbsp;</th><th>2004</th><th>2005</th><th>2006</th>*/}
+                    {/*</tr>*/}
+                    {themes.map((el)=>(
+                        <tr key={el.id}>
+                            <td>{el.title}</td>
+                            {el.Questions.map((el2) => (<td key={el2.id*1000} onClick={onPriceClick}><Button  variant="dark" ref={ref}>{el2.price}</Button></td>))}
+                        </tr>
+                    ))
+
+                    }
+                </table>
+            </div>
         </div>
     );
 };
