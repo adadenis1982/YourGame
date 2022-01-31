62679585323886e9bc8d940a63d9e9e35b6dc9b5
Sonya <sonya14tuzhikova@mail.ru>
2022-01-31 17:21:17 +0300

    list

--- game/src/components/Home/Home.js
+++ game/src/components/Home/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Table from '../Table/Table';
 
-function Home(props) {
+function Home() {
   const name = useSelector((state) => state.checkSessionReducer.user);
 
-  return <> {typeof name === 'string' ? `Приветствуем Вас ${name}` : 'Нет никого'} </>;
+  return (
+    <>
+      <p>{typeof name === 'string' ? `Приветствуем Вас ${name}` : 'Нет никого'}</p>
+      <Table></Table>
+    </>
+  )
 }
 
 export default Home;
